2756f3c00adb05b5cc61a66592198e99a2a087ec
Do1ore <100031663+Do1ore@users.noreply.github.com>
2023-08-23 18:28:52 +0300

    +User service

---
+++ MyPetAngularProject/src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+import {AppUser} from "../models/appUser";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private baseApiUrl = environment.baseApiUrl + 'api/User';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public searchUser(searchTerm: string): Observable<AppUser[]> {
+    const payload = {searchTerm: searchTerm}; // Используйте правильный ключ для searchTerm
+    return this.http.get<AppUser[]>(this.baseApiUrl + '/search-user' + '/' + searchTerm);
+  }
+}
